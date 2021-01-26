efcb67f24ab34478d598aa19e3c1f6f7aa0dfa4e
leeyoungak <slingstone7@gmail.com>
2021-01-27 02:54:43 +0900

    first commit

---
+++ src/main/java/net/summer/MySummerApplication.java
@@ -0,0 +1,13 @@
+package net.summer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySummerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySummerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/summer/MySummerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.summer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySummerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
